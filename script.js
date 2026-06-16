d5d17928ea0d89ba17a53e1a162211b9789082ae
dylan <dylanhuarca8@gmail.com>
2026-06-16 12:38:09 -0500

    agrego condicionales y saludo dinamico por hora

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Función
   function saludar(nombre) {
-    return "¡Hola " + nombre + ", bienvenido a mi portafolio!";
+    let hora = new Date().getHours();
+    let saludo;
+
+    if (hora < 12) {
+      saludo = "Buenos días";
+    } else if (hora < 18) {
+      saludo = "Buenas tardes";
+    } else {
+      saludo = "Buenas noches";
+    }
+
+    return saludo + " " + nombre + ", bienvenido a mi portafolio! 👋";
   }
 
   // Botón
